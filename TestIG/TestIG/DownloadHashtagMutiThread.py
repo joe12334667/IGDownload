d64263919f689de6c5dadd12e4bdb06d7f981964
joe12334667 <joe12334667@gmail.com>
2020-03-30 15:00:40 +0800

    修改.gitinore，MutiTheadDownload參數

--- TestIG/TestIG/DownloadHashtagMutiThread.py
+++ TestIG/TestIG/DownloadHashtagMutiThread.py
@@ -58,7 +58,7 @@ def is_all_chinese(strs):
     return True
 
 def is_all_chinese_And_English(strs):
-    ret_search = re.search("^[\u4e00-\u9fa5_a-zA-Z0-9]+$",strs) #掃描整個字串返回第一個匹配到的元素並結束，匹配不到返回None
+    ret_search = re.search("^[\u4e00-\u9fa5_a-zA-Z0-9\-]+$",strs) #掃描整個字串返回第一個匹配到的元素並結束，匹配不到返回None
     if(ret_search):
         return True
     return False
@@ -78,65 +78,67 @@ class Worker(threading.Thread):
             # 取得新的資料
             cat = self.queue.get()
             print("worker " , self.work , " ",cat)
-            ReQueue.put( DownloadHashtagsFromCategory(cat , self.num) )
+            self.requeue.put( DownloadHashtagsFromCategory(cat , self.num) )
 #--------------------------------------------------------------------------------------------------------------
 
-tStart = time.time()
-# 建立佇列
-CatQueue = queue.Queue(0)
-ReQueue = queue.Queue(0)
-category = "食"
-data = {category : []}
+def MutiTheadDownload(cate , RunTime):
 
-# 將資料放入佇列 
-with open(os.getcwd() + "/" + "#"+category+".txt", newline='' , encoding="utf-8" ) as csvfile:
-
-    # 讀取 CSV 檔案內容
-    rows = csv.reader(csvfile)
+    tStart = time.time()
+    # 建立佇列
+    CatQueue = queue.Queue(0)
+    ReQueue = queue.Queue(0)
+    category = cate
     data = {category : []}
-    #i = 0
-  # 以迴圈輸出每一列
-    for row in rows:
-        for item in row:
-            CatQueue.put(item)
 
-# 建立兩個 Worker
-my_worker1 = Worker(CatQueue, ReQueue , 100 , 1)
-my_worker2 = Worker(CatQueue, ReQueue , 100 , 2)
+    # 將資料放入佇列 
+    with open(os.getcwd() + "/" + "#"+category+".txt", newline='' , encoding="utf-8" ) as csvfile:
 
-# 讓 Worker 開始處理資料
-print("my_worker1 start")
-my_worker1.start()
-print("my_worker2 start")
-my_worker2.start()
+        # 讀取 CSV 檔案內容
+        rows = csv.reader(csvfile)
+        data = {category : []}
+        #i = 0
+      # 以迴圈輸出每一列
+        for row in rows:
+            for item in row:
+                CatQueue.put(item)
 
-# 等待所有 Worker 結束
-my_worker1.join()
-my_worker2.join()
+    # 建立兩個 Worker
+    my_worker1 = Worker(CatQueue, ReQueue , RunTime , 1)
+    my_worker2 = Worker(CatQueue, ReQueue , RunTime , 2)
+
+    # 讓 Worker 開始處理資料
+    print("my_worker1 start")
+    my_worker1.start()
+    print("my_worker2 start")
+    my_worker2.start()
+
+    # 等待所有 Worker 結束
+    my_worker1.join()
+    my_worker2.join()
 
 
-print("Done." )
-while ReQueue.qsize() > 0:
-    data[category].append(ReQueue.get())
+    print("Done." )
+    while ReQueue.qsize() > 0:
+        data[category].append(ReQueue.get())
 
 
-FileName = time.strftime("%Y-%m-%d_%H-%M") + category + ".json"
+    FileName = time.strftime("%Y-%m-%d_%H-%M") + category + ".json"
 
-# 資料夾與檔案是否存在
-if os.path.isfile(os.getcwd() + "/"+ category + "/" + FileName):
-    file =open(os.getcwd() + "/"+ category + "/" + FileName , mode = 'w' , encoding="utf-8")
-    print("檔案存在。")
+    # 資料夾與檔案是否存在
+    if os.path.isfile(os.getcwd() + "/"+ category + "/" + FileName):
+        file =open(os.getcwd() + "/"+ category + "/" + FileName , mode = 'w' , encoding="utf-8")
+        print("檔案存在。")
 
-else:
-    if not os.path.isdir(os.getcwd() + "/"+ category):
-        os.mkdir(category)
-    file = open(os.getcwd() +"/" + category+ "/" + FileName , mode = 'w' , encoding="utf-8")
-    print("檔案不存在，已創立" + FileName  )
+    else:
+        if not os.path.isdir(os.getcwd() + "/"+ category):
+            os.mkdir(category)
+        file = open(os.getcwd() +"/" + category+ "/" + FileName , mode = 'w' , encoding="utf-8")
+        print("檔案不存在，已創立" + FileName  )
 
-# 寫入json檔並調整格式
-file.write(json.dumps(data,ensure_ascii=False , indent=4, separators=(',', ': ')))
-file.close()
+    # 寫入json檔並調整格式
+    file.write(json.dumps(data,ensure_ascii=False , indent=4, separators=(',', ': ')))
+    file.close()
     
-tEnd = time.time()
-print("time : %f s" % (tEnd - tStart))
+    tEnd = time.time()
+    print("time : %f s" % (tEnd - tStart))
 
--- TestIG/TestIG/module2.py
+++ TestIG/TestIG/module2.py
@@ -1,6 +1,2 @@
-import re
-ret_search = re.search("^[\u4e00-\u9fa5_a-zA-Z0-9]+$","陳joe123") #掃描整個字串返回第一個匹配到的元素並結束，匹配不到返回None
-if(ret_search):
-    print("yes")
-else:
-    print("not")
+import DownloadHashtagMutiThread
+DownloadHashtagMutiThread.MutiTheadDownload("食" ,10)
