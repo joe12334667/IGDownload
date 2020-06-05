57eeb4fceb00c724a45affb4f4df972c3c8cbfde
joe12334667 <joe12334667@gmail.com>
2020-06-05 12:03:09 +0800

    Create module3.py

---
+++ TestIG/TestIG/module3.py
@@ -0,0 +1,5 @@
+
+
+str = "【New Item !】\n洗水牛仔短裙\n復古色調和下擺小開叉，提升了時尚感！\n牛仔單品亦是四季常用的Item！\n-\nColor: Indigo\nSize: Free\nTexture: Cotton\n-\n#fashion #style #photooftheday\n#instagood #ootd #outfitoftoday\n #summer #tokyo #style #offshoulder\n#street #shibuya #fashion #ribbons \n#2020春夏  #instafashion #girl #hkig #hkgirl #d890-10"
+str = str.replace("#d890-10" , "")
+print(str)
